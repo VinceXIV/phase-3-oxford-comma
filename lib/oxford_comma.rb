cbc1378332932c6d9dfaeba75f7f6a22eb9fe9b6
Vincent <makokhavomondi@gmail.com>
2022-10-19 21:20:20 +0300

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    return array[0] unless array.length > 1
+    return "#{array[0]} and #{array[1]}" unless array.length > 2
+    array.slice(0, array.length - 1).join(", ") +", and #{array[-1]}"
 end
+
